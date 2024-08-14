1bc842432fd02e9ec7ea05585f4b983f841c2807
Wassef05 <wassef.bhr@gmail.com>
2024-08-14 11:44:43 +0100

    navbarchanges2

--- client/src/components/NavbarComp.jsx
+++ client/src/components/NavbarComp.jsx
@@ -122,7 +122,7 @@ export default function NavbarComp() {
                 <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
                   <li>
                     <a href="/searchProject?filter=future" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
-                      Nos Projet Non Réalisées
+                      Nos future projets
                     </a>
                   </li>
                   <li>
