3745dd1f5f02fb112b1fe49b95f8f560cf8313ac
Wassef05 <wassef.bhr@gmail.com>
2024-09-14 23:39:55 +0100

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
+    host: '0.0.0.0', // Écoute sur toutes les interfaces
+    port: process.env.PORT || 5173, // Utilise le port fourni par Render ou 5173 par défaut
     proxy: {
       '/api': {
         target: 'http://localhost:4000',
@@ -18,11 +20,11 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'], // Example: move react and react-dom to a separate chunk
-          // Add other modules here as needed
+          vendor: ['react', 'react-dom'], // Exemple : séparer react et react-dom en un autre chunk
+          // Ajoutez d'autres modules ici si nécessaire
         },
       },
     },
-    chunkSizeWarningLimit: 1000, // Increase the limit to 1000 kB
+    chunkSizeWarningLimit: 1000, // Augmentez la limite à 1000 kB
   },
 });
