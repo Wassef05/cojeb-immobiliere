e1dc8b19398dafb7832a2d5d86bfa1bfd656b953
Wassef05 <wassef.bhr@gmail.com>
2024-09-18 21:50:45 +0100

    commit finall

--- client/src/components/AddedComponents/AboutHeader.jsx
+++ client/src/components/AddedComponents/AboutHeader.jsx
@@ -1,91 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { HiHome } from "react-icons/hi";
-import axios from 'axios';
+import React, { useEffect } from "react";
 import cover from "../../img/cover1.jpg";
 import AOS from "aos";
 import "aos/dist/aos.css";
-// import { Breadcrumb } from "flowbite-react";
-import logement from "../../img/logement.png"
-import commerce from "../../img/commercial.png"
-import parking from "../../img/parking.png"
-
-const fetchPostCount = async () => {
-  try {
-    const response = await axios.get('/api/posts/count');
-    return response.data.count;
-  } catch (error) {
-    console.error("Erreur lors de la récupération du nombre de posts", error);
-    return 0;
-  }
-}
-
-const fetchProjectCount = async () => {
-  try {
-    const response = await axios.get('/api/projects/count');
-    return response.data.count;
-  } catch (error) {
-    console.error("Erreur lors de la récupération du nombre de projets", error);
-    return 0;
-  }
-}
-
-const fetchPostParkingCount = async () => {
-  try {
-    const response = await axios.get('/api/posts/countParking');
-    return response.data.count;
-  } catch (error) {
-    console.error("Erreur lors de la récupération du nombre de parkings posts", error);
-    return 0;
-  }
-}
-
-const fetchProjectParkingCount = async () => {
-  try {
-    const response = await axios.get('/api/projects/countParking');
-    return response.data.count;
-  } catch (error) {
-    console.error("Erreur lors de la récupération du nombre de parkings projets", error);
-    return 0;
-  }
-}
+import StateCard from "./StateCardAbout";
 
 const AboutHeader = () => {
-  const [projectCount, setProjectCount] = useState(0);
-  const [projectParkingCount, setProjectParkingCount] = useState(0);
-
-
-  const [displayedProjectCount, setDisplayedProjectCount] = useState(0);
-  const [displayedParkingCount, setDisplayedParkingCount] = useState(0);
-
-  const animateValue = (start, end, setter) => {
-    let current = start;
-    const increment = Math.ceil((end - start) / 100);
-    const duration = 2000;
-    const stepTime = Math.abs(Math.floor(duration / (end - start)));
-    
-    const timer = setInterval(() => {
-      current += increment;
-      setter(Math.min(current, end));
-      if (current >= end) {
-        clearInterval(timer);
-      }
-    }, stepTime);
-  };
-
   useEffect(() => {
-    const getCounts = async () => {
-      const projectCount = await fetchProjectCount();
-      const projectParkingCount = await fetchProjectParkingCount();
-
-      setProjectCount(projectCount);
-      setProjectParkingCount(projectParkingCount);
-
-      animateValue(0, projectCount, setDisplayedProjectCount);
-      animateValue(0,  projectParkingCount, setDisplayedParkingCount);
-    };
-    
-    getCounts();
-
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
@@ -102,74 +22,31 @@ const AboutHeader = () => {
           className="absolute inset-0 w-full h-full object-cover"
         />
 
-        <div className="min-h-[350px] relative z-20 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-black p-6" data-aos="fade-down">
+        <div
+          className="min-h-[350px] relative z-20 h-full max-w-6xl mx-auto flex flex-col justify-center items-center text-center text-black p-6"
+          data-aos="fade-down"
+        >
           <h2 className="sm:text-4xl uppercase text-3xl font-bold mb-6">
             A propos
           </h2>
-        
-        </div>
-      </div>
-      <div className="grid items-center justify-center -mt-10 mb-14 mx-auto max-w-[70vw] sm:max-w-[80vw] lg:grid-cols-3 gap-4 z-20 relative lg:left-16 max-lg:px-4">
-      <div className="  flex flex-auto items-center justify-center -mt-40 " data-aos="fade-up" data-aos-delay="200">
-
-
-
-
-      </div>
-      </div>
-
-
-
-
-      <div className="grid pb-6 -mt-10 mb-14 mx-auto max-w-[50vw] sm:max-w-[60vw] lg:grid-cols-3 z-20 relative max-lg:px-4 bg-gradient-to-r from-gray-300 to-gray-400 rounded-[30px] shadow-lg shadow-black" data-aos="fade-up" data-aos-delay="200">
-        
-
-
-
-
-
-        <div className="flex lg:w-[18vw] flex-col items-center justify-center rounded-lg w-full h-44 p-4 text-center  " >
-        <div className="pt-4 p-0 sm:p-4 md:p-6 grid grid-cols-3 items-center justify-center ">
-        <img src={logement} className='col-span-1'/>
-        <div className='col-span-2'>
-        <h3 className="text-xs text-black sm:text-base text-center md:text-lg font-semibold uppercase mb-2 ml-4 lg:mt-2">logements</h3><h3 className="text-lg md:text-xl text-black font-semibold mb-2 mt-6">{displayedProjectCount} <span className='text-black'>+</span></h3>
-
-        </div>
-          </div>
-        </div>
-
-
-        <div className="flex lg:w-[18vw] flex-col items-center justify-center rounded-lg w-full h-44 p-4 text-center  ">
-        <div className="p-0 md:p-6 
-        grid grid-cols-3 items-center">
-        <img src={commerce} className='col-span-1'/>
-        <div className='col-span-2'>
-        <h3 className="text-xs sm:text-base mt-3 text-center md:text-lg text-black font-semibold uppercase mb-2">espace commercial</h3>
-        <h3 className="text-lg md:text-xl font-semibold mb-2 mt-6 text-black">{displayedProjectCount} <span className='text-black'>+</span></h3>
-        </div>
-          </div>
-        </div>
-
-
-        <div className="flex lg:w-[18vw] flex-col items-center justify-center rounded-lg w-full h-44 p-4 text-center  " >
-        <div className="p-0 sm:p-4 md:p-6   items-center grid grid-cols-3">
-        <img src={parking} className='col-span-1'/>
-              <div className='col-span-2'>
-              <h3 className="text-xs sm:text-base mt-3 text-center md:text-lg text-black font-semibold uppercase mb-2">Parking</h3>
-            <h3 className="text-lg md:text-xl font-semibold mb-2 mt-6 text-black">{displayedParkingCount} <span className='text-black'>+</span></h3>
-         
-              </div>
-             </div>
-        </div>
-
-
         </div>
       </div>
 
+      {/* Container for the StateCard */}
+      <div className="relative z-20 -mt-24 mb-16 flex justify-center items-center">
+        {/* Adjust -mt-24 for the vertical placement above the image */}
+        <StateCard />
+      </div>
 
-
-
+      <div className="grid items-center justify-center mb-16 mx-auto max-w-[70vw] sm:max-w-[80vw] lg:grid-cols-3 gap-4 z-20 relative lg:left-16 max-lg:px-4">
+        <div
+          className="flex flex-auto items-center justify-center"
+          data-aos="fade-up"
+          data-aos-delay="200"
+        ></div>
+      </div>
+    </div>
   );
-}
+};
 
 export default AboutHeader;
--- client/src/components/AddedComponents/Card1About.jsx
+++ client/src/components/AddedComponents/Card1About.jsx
@@ -4,48 +4,48 @@ import cov from '../../img/card1.png';
 export default function Card1About() {
   return (
     <div className="bg-[#E6EEF3]">
-    <div 
-      className="bg-[#E6EEF3] shadow-lg shadow-gray-500/40  h-auto grid place-items-center max-h-full max-w-[100vw] sm:pt-40 mx-auto sm:p-32 p-6 font-[sans-serif]"
-      data-aos="fade-up"
-    >
-      <div className="grid md:grid-cols-2 items-center gap-6">
-        <div 
-          className="md:flex md:justify-center"
-          data-aos="zoom-in"
-          data-aos-delay="200"
-        >
-          <img
-            src={cov}
-            className="rounded-lg sm:w-2/3 md:w-[300px] lg:w-[500px] shadow-lg shadow-gray-900/70"
-            alt="Profile"
-            data-aos="fade-right"
-            data-aos-delay="400"
-          />
-        </div>
-        <div 
-          className="md:col-span-1"
-          data-aos="fade-left"
-          data-aos-delay="600"
-        >
-          <div className="mb-12 sm:max-w-[50vw]">
-           <h2
-className="text-[#EB5E1A] font-semibold sm:text-4xl font-playfair mb-16 "
-data-aos="fade-down"
+      <div 
+        className="bg-[#E6EEF3] shadow-lg shadow-gray-500/40  h-auto grid place-items-center max-h-full max-w-[100vw] sm:pt-40 mx-auto sm:p-32 p-6 font-[sans-serif]"
+        data-aos="fade-up"
+      >
+        <div className="grid md:grid-cols-2 items-center gap-6">
+          <div 
+            className="md:flex md:justify-center"
+            data-aos="zoom-in"
+            data-aos-delay="200"
+          >
+            <img
+              src={cov}
+              className="rounded-lg sm:w-2/3 md:w-[300px] lg:w-[500px] shadow-lg shadow-gray-900/70"
+              alt="Profile"
+              data-aos="fade-right"
+              data-aos-delay="400"
+            />
+          </div>
+          <div 
+            className="md:col-span-1"
+            data-aos="fade-left"
+            data-aos-delay="600"
+          >
+            <div className="mb-12 sm:max-w-[50vw]">
+             <h2
+  className="text-[#A95834]   font-averia font-bold text-5xl mb-16  "
+  data-aos="fade-down"
 >
-A PROPOS DE NOUS
+  A PROPOS DE NOUS
 </h2>
 
-            <p 
-              className="leading-relaxed text-gray-800 leading-loose font-playfair text-base text-justify"
-              data-aos="fade-up"
-              data-aos-delay="800"
-            >
-              La société COGEB IMMOBILIERE, fondée en <strong> 1999 </strong>et dirigée par Monsieur <strong>Mohamed LAZREG</strong> , est agréée par le ministère de l’équipement de l’habitat et de l’aménagement du territoire. Son objectif principal est de développer des projets immobiliers de haute qualité en sélectionnant soigneusement les sites, en concevant des espaces répondant aux besoins des clients, et en garantissant un cadre de vie amélioré. Elle dispose d'une équipe compétente, notamment des architectes, bureaux d'études et de contrôle, qui accordent une attention particulière à l'architecture de ses bâtiments, devenus des références dans le domaine.
-            </p>
+              <p 
+                className="  leading-relaxed text-gray-800 leading-loose font-playfair text-base text-justify"
+                data-aos="fade-up"
+                data-aos-delay="800"
+              >
+                La société <strong> COGEB IMMOBILIERE</strong>, fondée en <strong>1999</strong> et dirigée par Monsieur <strong> Mohamed LAZREG</strong>, est agréée par le ministère de l’équipement de l’habitat et de l’aménagement du territoire. Son objectif principal est de développer des projets immobiliers de haute qualité en sélectionnant soigneusement les sites, en concevant des espaces répondant aux besoins des clients, et en garantissant un cadre de vie amélioré. Elle dispose d'une équipe compétente, notamment des architectes, bureaux d'études et de contrôle, qui accordent une attention particulière à l'architecture de ses bâtiments, devenus des références dans le domaine.
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-  )
+  );
 }
